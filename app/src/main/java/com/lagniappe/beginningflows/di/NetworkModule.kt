c6191226f791948174c88414171240aa1b0a45a9
Ferrol Blackmon <ferrol.blackmon@gmail.com>
2026-03-16 09:28:43 -0400

    adding missing providers for retrofit and service

---
+++ app/src/main/java/com/lagniappe/beginningflows/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.lagniappe.beginningflows.di
+
+import com.lagniappe.beginningflows.data.remote.RickAndMortyService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://rickandmortyapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRickAndMortyService(retrofit: Retrofit): RickAndMortyService {
+        return retrofit.create(RickAndMortyService::class.java)
+    }
+}
